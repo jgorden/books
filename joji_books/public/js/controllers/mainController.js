bc4cb00b93f833b28446ad80add508b9b196fca1
jgorden <joji.gorden@gmail.com>
2016-03-08 15:59:19 -0800

    add book covers from goodreads/open library

--- joji_books/public/js/controllers/mainController.js
+++ joji_books/public/js/controllers/mainController.js
@@ -1,13 +1,15 @@
 app.controller('mainController', ['$scope', '$http', function($scope, $http){
   $scope.test = 'hey it works';
   console.log('is it working')
-  // $scope.home_page = function($http){
-  //   $http.get('/').success(function(){
-  //     console.log('woooooo');
-  //   });
-  // };
-  // $scope.home_page($http);
 
+  $scope.imageLink = function(book){
+    if (book.book.image_url === 'http://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png'){
+      return 'http://covers.openlibrary.org/b/isbn/' + book.book.isbn + '-M.jpg'
+    }
+    else {
+      return book.book.image_url
+    }
+  }
 
   $http.get('/users').success(function(r){
       if (r) {
